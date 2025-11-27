abfaa8c628100beb56403e6374fd0079b0dc8f26
John Kladis <yanniskladis@hotmail.com>
2025-11-27 11:30:31 +0200

    product-Items, errors and loading

---
+++ client/src/app/core/services/busy.service.ts
@@ -0,0 +1,22 @@
+import { Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BusyService {
+  loading = false;
+  busyRequestCount = 0;
+
+  busy() {
+    this.busyRequestCount++;
+    this.loading = true;
+  }
+
+  idle() {
+    this.busyRequestCount--;
+    if (this.busyRequestCount <= 0) {
+      this.busyRequestCount = 0;
+      this.loading = false;
+    }
+  }
+}

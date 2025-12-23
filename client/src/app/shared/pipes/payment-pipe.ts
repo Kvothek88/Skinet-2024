3dab8e319eacdf54860cf7755efed8638dd8aa37
John Kladis <yanniskladis@hotmail.com>
2025-12-23 12:06:51 +0200

    SignalR API

--- client/src/app/shared/pipes/payment-pipe.ts
+++ client/src/app/shared/pipes/payment-pipe.ts
@@ -1,15 +1,21 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { ConfirmationToken } from '@stripe/stripe-js';
+import { PaymentSummary } from '../models/order';
 
 @Pipe({
   name: 'payment'
 })
 export class PaymentPipe implements PipeTransform {
 
-  transform(value?: ConfirmationToken['payment_method_preview'], ...args: unknown[]): unknown {
-    if (value?.card) {
-      const {brand, exp_month, exp_year, last4} = value.card;
+  transform(value?: ConfirmationToken['payment_method_preview'] | PaymentSummary, ...args: unknown[]): unknown {
+    if (value && 'card' in value) {
+      const {brand, exp_month, exp_year, last4} = 
+        (value as ConfirmationToken['payment_method_preview']).card!;
       return `${brand.toUpperCase()} **** **** **** ${last4}, Exp: ${String(exp_month).padStart(2, '0')}/${exp_year}`;
+    } else if (value && 'last4' in value){
+      const {brand, expMonth, expYear, last4} = 
+        value as PaymentSummary;
+      return `${brand.toUpperCase()} **** **** **** ${last4}, Exp: ${String(expMonth).padStart(2, '0')}/${expYear}`;      
     } else {
       return 'Unknown card';
     }

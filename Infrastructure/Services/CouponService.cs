ffbaa733084a35bffd7e42e276d13bfc58d91763
John Kladis <yanniskladis@hotmail.com>
2025-12-26 19:35:36 +0200

    Fixed coupon bug

--- Infrastructure/Services/CouponService.cs
+++ Infrastructure/Services/CouponService.cs
@@ -15,20 +15,24 @@ public class CouponService : ICouponService
     public async Task<AppCoupon?> GetCouponFromPromoCode(string code)
     {
         var promotionService = new PromotionCodeService();
+        var couponService = new Stripe.CouponService();
         var options = new PromotionCodeListOptions
         {
             Code = code
         };
         var promotionCodes = await promotionService.ListAsync(options);
         var promotionCode = promotionCodes.FirstOrDefault();
-        if (promotionCode != null && promotionCode.Promotion.Coupon != null)
+
+        var coupon = await couponService.GetAsync(promotionCode.Promotion.CouponId);
+
+        if (promotionCode != null && coupon != null)
         {
             return new AppCoupon
             {
-                Name = promotionCode.Promotion.Coupon.Name,
-                AmountOff = promotionCode.Promotion.Coupon.AmountOff,
-                PercentOff = promotionCode.Promotion.Coupon.PercentOff,
-                CouponId = promotionCode.Promotion.Coupon.Id,
+                Name = coupon.Name,
+                AmountOff = coupon.AmountOff,
+                PercentOff = coupon.PercentOff,
+                CouponId = coupon.Id,
                 PromotionCode = promotionCode.Code
             };
         }
